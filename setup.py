04dc33cef3ab979374732080f223efc88a07c788
Developer Ashish <therobomarket@gmail.com>
2022-07-16 14:37:41 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.command.install_data import install_data
+from gettext import install
+from http.client import PROXY_AUTHENTICATION_REQUIRED
+from pickle import LONG_BINGET
+from platform import python_branch
+from re import A
+from struct import pack
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+setup(
+    name ="stc",
+    version = "0.0.1",
+    author = "Developer Ashish",
+    description = "A small package for dvc ml pipeline demo",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/pydeveloperashish/dvc-ml-mlops",
+    author_email = "therobomarket@gmail.com",
+    packages = ["src"],
+    python_requires = ">=3.7",
+    install_requires = [
+        "dvc", "pandas", "scikit-learn", "joblib"
+                       ]
+    )    
