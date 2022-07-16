a6217070a969a906279fef0799139d73b836347e
Developer Ashish <therobomarket@gmail.com>
2022-07-16 20:47:57 +0530

    stage_02 added

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -27,7 +27,7 @@ def get_data(config_path):
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--config", "-c", default = "config/config.yaml")
     parsed_args = args.parse_args()
     get_data(config_path = parsed_args.config)
      
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -12,3 +12,8 @@ def create_directory(dirs: list):
     for dir_path in dirs:
        os.makedirs(dir_path, exist_ok=True) 
        print(f"directory is created at {dir_path}")
+       
+
+def save_local_df(data, data_path):
+    data.to_csv(data_path, index = False)
+    print(f"data is saved at {data_path}")
