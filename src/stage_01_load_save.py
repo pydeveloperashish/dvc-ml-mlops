7af8bdcd1ab9299301aa2826a43af8d3465fbae9
Developer Ashish <therobomarket@gmail.com>
2022-07-16 15:52:28 +0530

    stage 1 created

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,35 @@
+from readline import remove_history_item
+from src.utils.all_utils import create_directory, read_yaml
+import argparse
+import os
+import pandas as pd
+from pprint import pprint
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+    remote_data_path = config["data_source"]
+    df = pd.read_csv(remote_data_path)
+    
+    # save dataset in local storage
+    # create path to directory: artifacts/raw_local_dir/data.csv
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    raw_local_dir = config["artifacts"]["raw_local_dir"]
+    raw_local_file = config["artifacts"]["raw_local_file"]
+    
+    raw_local_dir_path = os.path.join(artifacts_dir, raw_local_dir)
+    create_directory(dirs = [raw_local_dir_path])
+    
+    raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
+    df.to_csv(raw_local_file_path, sep = ',', index = False)
+    
+    #print(raw_local_file_path)
+    
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="config/config.yaml")
+    parsed_args = args.parse_args()
+    get_data(config_path = parsed_args.config)
+     
+    
