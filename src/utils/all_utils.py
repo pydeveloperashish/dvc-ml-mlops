8853a089a093c154af9ffd63a5623c979bc62f49
Developer Ashish <therobomarket@gmail.com>
2022-07-16 14:12:08 +0530

    stage 01 utils added

---
+++ src/utils/all_utils.py
@@ -0,0 +1,8 @@
+from email import contentmanager
+import yaml
+import os
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    return content
