59fd3a27bf8e7635fe72adc5d47f9b1c1dfcce2a
Ashish Kushwaha <59412013+pydeveloperashish@users.noreply.github.com>
2022-07-18 13:28:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from distutils.command.install_data import install_data
-from gettext import install
-from http.client import PROXY_AUTHENTICATION_REQUIRED
-from pickle import LONG_BINGET
-from platform import python_branch
-from re import A
-from struct import pack
 from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as f:
